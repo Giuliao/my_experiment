ad75fcca0e1e27f8caac346607d41c9c6f330251
Evi1G <happy_wggg@163.com>
2017-07-20 19:46:39 +1000

    update

--- CheckRobstness.py
+++ CheckRobstness.py
@@ -1,27 +1,51 @@
 # -*-coding:utf-8 -*-
+# data can be viewed in 1.png:
+# 0 0 0 0 1 1 1 0
+# 0 0 0 1 1 1 0 1
+# 0 0 0 1 1 0 1 1
+# 0 1 1 0 0 0 0 1
+# 1 1 1 0 0 1 1 1
+# 1 1 0 0 1 0 1 1
+# 1 0 1 0 1 1 0 1
+# 0 1 1 1 1 1 1 0
 
+# result:
+# no remove, (3, 1)-robust
+# when remove node1, (3, 1)-robust
+# when remove node2, (2, 1)-robust
+# when remove node3, (2, 2)-robust
+# when remove node4, (3, 1)-robust
+# when remove node5, (2, 2)-robust
+# when remove node6, (2, 2)-robust
+# when remove node7, (2, 2)-robust
+# when remove node7, (2, 1)-robust
 
 import numpy as np
 import itertools
 import math
 
+
 def robust_holds(A, S1, S2, r, s):
     isRSRobust = False
 
-    def calc_reachable(r, S):
+    def calc_reachable(S):
         ss = 0
+        # iterate the node(>0) in S
         for i in S:
-            t = 0
+            t = 0 # count for the node that owns the neighbors outside S
             for j in range(A.shape[0]):
+                # iterate the column of Matrix A to check the in-degree of node i
+                # it' s worth noting that in Determine_partitial_roubst the node i is not included in S,
+                # but when calc the in-degree it's still there.
                 if A[j][i-1] != 0 and (j+1) not in S:
                     t += 1
             if t >= r:
                 ss += 1
         return ss
 
-    if calc_reachable(r, S1) == len(S1) \
-            or calc_reachable(r, S2) == len(S2) \
-            or calc_reachable(r, S1) + calc_reachable(r, S2) >= s:
+    if calc_reachable(S1) == len(S1) \
+            or calc_reachable(S2) == len(S2) \
+            or calc_reachable(S1) + calc_reachable(S2) >= s:
             isRSRobust = True
 
     return isRSRobust
@@ -47,16 +71,16 @@ def check_robustness(A, r, s):
     return isRSRobust, None, None
 
 
-def determine_robustness(A):
+def calc_in_degree(A):
+    for i in range(A.shape[0]):
+        count = 0
+        for j in range(A.shape[0]):
+            if A[j][i] !=0:
+                count += 1
+        yield count
 
-    def calc_in_degree(A):
-        for i in range(A.shape[0]):
-            count = 0
-            for j in range(A.shape[0]):
-                if A[j][i] !=0:
-                    count += 1
-            yield count
-
+# the func is the same as the determine_robustness
+def determine_robustness2(A):
     r = min(calc_in_degree(A))
     r = min(r, math.ceil(A.shape[0]*1.0/2.0))
     s = A.shape[0]
@@ -80,13 +104,58 @@ def determine_robustness(A):
     return r, s
 
 
+def determine_robustness(A):
+    return determine_partial_robust(A, None)
+
+
+# i is more than 0
+def determine_partial_robust(A, i):
+    r = min(calc_in_degree(A))
+    r = min(r, math.ceil(A.shape[0] * 1.0 / 2.0))
+    s = A.shape[0]
+    n = A.shape[0]  # the number of vertex
+
+    flag = 1 if not i else 0 # set the upper bound of the K set
+
+    # partition the set with k nodes, k at least 2 because of for 1-robust
+    for k in range(2, n + flag):
+        # combinition set of which the number is k
+        for K in itertools.combinations([t for t in range(1, n + 1) if t != i], k):
+            # subsets of K both of which are nonempty
+            for S1, S2 in nonempty_subset(K):
+                isRSRoubst = robust_holds(A, S1, S2, r, s)
+                if not isRSRoubst and s > 0:
+                    s -= 1
+            while (not isRSRoubst) and r > 0:
+                while (not isRSRoubst) and s > 0:
+                    isRSRoubst = robust_holds(A, S1, S2, r, s)
+                    if not isRSRoubst:
+                        s -= 1
+                if not isRSRoubst:
+                    r -= 1
+                    s = n
+            if r == 0:
+                return r, s
+    return r, s
+
+
 if __name__ == '__main__':
     with open('data3.in', 'r') as f:
         A = []
+        B = []
+        count = 0
         for line in f.readlines():
-            A.append(line.split(' '))
+            if count < 8:
+                A.append(line.strip().split(' '))
+            else:
+                B.append(line.strip().split(' '))
+            count += 1
         # print check_robustness(np.array(A, dtype=np.int), 3, 3)
-        print determine_robustness(np.array(A, dtype=np.int))
+        print np.array(B, dtype=np.int)
+        print np.array(A, dtype=np.int)
+        print determine_robustness2(np.array(B, dtype=np.int))
+        print determine_robustness(np.array(B, dtype=np.int))
+
 
 
 
